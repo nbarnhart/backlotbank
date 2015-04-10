34df43c5b59fe669f8feb1ac6037c66b5768e709
hoonto <mmullens@hoonto.com>
2015-04-10 09:16:34 -0400

    updating for ssl

--- server/boot/explorer.js
+++ server/boot/explorer.js
@@ -18,7 +18,10 @@ module.exports = function mountLoopBackExplorer(server) {
   var explorerApp = explorer(server, { basePath: restApiRoot });
   server.use('/explorer', explorerApp);
   server.once('started', function() {
-    var baseUrl = server.get('url').replace(/\/$/, '');
+
+    var baseUrl = 'http://' + server.get('host') + ':' + server.get('port');
+
+    //var baseUrl = server.get('url').replace(/\/$/, '');
     // express 4.x (loopback 2.x) uses `mountpath`
     // express 3.x (loopback 1.x) uses `route`
     var explorerPath = explorerApp.mountpath || explorerApp.route;
